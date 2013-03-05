0db6008c1acda8e1d93f7195491c6d5de1e3511c
Bastian Feder <github@bastian-feder.de>
2013-03-05 16:39:39 +0100

    Tested: added a test to verify the funcitonality of implemented destroy()

--- Tests/Liip/Drupal/Modules/Registry/Memory/PopoTest.php
+++ Tests/Liip/Drupal/Modules/Registry/Memory/PopoTest.php
@@ -5,5 +5,20 @@ use Liip\Drupal\Modules\Registry\Tests\RegistryTestCase;
 
 class PopoTest extends RegistryTestCase
 {
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::destroy
+     */
+    public function testDestroy()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
 
+        $registry->register('Tux', 'devil');
+        $registry->destroy();
+
+        $this->assertAttributeEquals(array(), 'registry', $registry);
+    }
 }
