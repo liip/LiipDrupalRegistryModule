a6cc6aa9d5733105eceb910c00688298eb6e6471
Bastian Feder <github@bastian-feder.de>
2013-03-16 12:50:57 +0100

    Fixed behavior of the memory registry.

--- Tests/Liip/Drupal/Modules/Registry/Memory/PopoTest.php
+++ Tests/Liip/Drupal/Modules/Registry/Memory/PopoTest.php
@@ -7,6 +7,7 @@ class PopoTest extends RegistryTestCase
 {
     /**
      * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::destroy
+     * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::__construct
      */
     public function testDestroy()
     {
@@ -20,5 +21,241 @@ class PopoTest extends RegistryTestCase
         $registry->destroy();
 
         $this->assertAttributeEquals(array(), 'registry', $registry);
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::init
+     */
+    public function testInit()
+    {
+        $registry = new Popo(
+            'Tux',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+        $registry->init();
+
+        $this->assertAttributeEquals(array('Tux' =>  array()), 'registry', $registry);
+    }
+
+    /**
+     * @expectedException \Liip\Drupal\Modules\Registry\RegistryException
+     * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::init
+     */
+    public function testInitExpectingException()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+        $registry->register('Darwin', 'Evolution works!');
+        $registry->init();
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::getContent
+     */
+    public function testGetContent()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+
+        $registry->register('Tux', array('devil', 'Beastie'));
+
+        $this->assertEquals(
+            array('Tux' => array('devil', 'Beastie')),
+            $registry->getContent()
+        );
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::getContentById
+     */
+    public function testGetContentById()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+
+        $registry->register('Tux', array('devil', 'Beastie'));
+
+        $this->assertEquals(
+            array('devil', 'Beastie'),
+            $registry->getContentById('Tux')
+        );
+    }
+
+   /**
+    * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::register
+    */
+    public function testRegister()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+
+        $registry->register('Tux', array('devil', 'Beastie'));
+
+        $this->assertAttributeEquals(
+            array('mySection' =>
+                array(
+                    'Tux' => array('devil', 'Beastie')
+                )
+            ),
+            'registry',
+            $registry
+        );
+    }
+
+   /**
+    * @expectedException \Liip\Drupal\Modules\Registry\RegistryException
+    * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::register
+    */
+    public function testRegisterExpectingException()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+
+        $registry->register('Tux', array('devil', 'Beastie'));
+        $registry->register('Tux', array('Beastie'));
+    }
+
+   /**
+    * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::replace
+    */
+    public function testReplace()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+
+        $registry->register('Tux', array('devil', 'Beastie'));
+        $registry->replace('Tux', array('Beastie'));
+
+        $this->assertAttributeEquals(
+            array('mySection' =>
+                array(
+                    'Tux' => array('Beastie')
+                )
+            ),
+            'registry',
+            $registry
+        );
+    }
+
+   /**
+    * @expectedException \Liip\Drupal\Modules\Registry\RegistryException
+    * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::replace
+    */
+    public function testReplaceExpectingException()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+
+        $registry->replace('Tux', array('Beastie'));
+    }
+
+   /**
+    * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::unregister
+    */
+    public function testUnregister()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+
+        $registry->register('Tux', array('devil', 'Beastie'));
+        $registry->unregister('Tux');
+
+        $this->assertAttributeEquals(
+            array('mySection' => array()),
+            'registry',
+            $registry
+        );
+    }
+
+   /**
+    * @expectedException \Liip\Drupal\Modules\Registry\RegistryException
+    * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::unregister
+    */
+    public function testUnregisterExpectingExcetion()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+
+        $registry->unregister('Tux');
+    }
+
+   /**
+    * @covers \Liip\Drupal\Modules\Registry\Memory\Popo::isRegistered
+    */
+    public function testIsRegistered()
+    {
+        $registry = new Popo(
+            'mySection',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+
+        $this->assertFalse($registry->isRegistered('Tux'));
+    }
+
+    /**
+     * @covers \stdClass
+     */
+    public function testLiveCycle()
+    {
+        $registry = new Popo(
+            'Tux',
+            $this->getDrupalCommonConnectorMock(),
+            $this->getAssertionObjectMock()
+        );
+
+        $registry->init();
+        $this->assertAttributeEquals(array('Tux' => array()), 'registry', $registry);
+
+        $registry->register('Devil', array('os' => 'Linux'));
+        $this->assertAttributeEquals(
+            array(
+                'Tux' => array('Devil' => array('os' => 'Linux'))
+            ),
+            'registry',
+            $registry
+        );
+
+        $data = $registry->getContent();
+        $this->assertEquals(array('Devil' => array('os' => 'Linux')), $data);
+
+        $entry = $registry->getContentById('Devil');
+        $this->assertEquals(array('os' => 'Linux'), $entry);
+
+        $registry->unregister('Devil');
+        $this->assertAttributeEquals(array('Tux' => array()), 'registry', $registry);
+
+        $registry->destroy();
+        $this->assertAttributeEquals(array(), 'registry', $registry);
+
+
     }
 }
--- src/Liip/Drupal/Modules/Registry/Memory/Popo.php
+++ src/Liip/Drupal/Modules/Registry/Memory/Popo.php
@@ -1,11 +1,26 @@
 <?php
 namespace Liip\Drupal\Modules\Registry\Memory;
 
+use Assert\Assertion;
+use Liip\Drupal\Modules\DrupalConnector\Common;
 use Liip\Drupal\Modules\Registry\Registry;
+use Liip\Drupal\Modules\Registry\RegistryException;
 
 
 class Popo extends Registry
 {
+    /**
+     * @param string $section
+     * @param \Liip\Drupal\Modules\DrupalConnector\Common $dcc
+     * @param \Assert\Assertion $assertion
+     */
+    public function __construct($section, Common $dcc, Assertion $assertion)
+    {
+        parent::__construct($section, $dcc, $assertion);
+
+        $this->registry[$this->section] = array();
+    }
+
     /**
      * Shall delete the current registry from the database.
      */
@@ -21,7 +36,7 @@ class Popo extends Registry
      */
     public function init()
     {
-        if(! empty($this->registry)) {
+        if(! empty($this->registry[$this->section])) {
             throw new RegistryException(
                 $this->drupalCommonConnector->t(RegistryException::DUPLICATE_INITIATION_ATTEMPT_TEXT),
                 RegistryException::DUPLICATE_INITIATION_ATTEMPT_CODE
@@ -29,5 +44,104 @@ class Popo extends Registry
         }
 
         $this->registry[$this->section] = array();
+    }
+
+    /**
+     * Provides the current set of registered items.
+     *
+     * @return array
+     */
+    public function getContent()
+    {
+        return $this->registry[$this->section];
+    }
+
+    /**
+     * Finds the item corresponding to the provided identifier in the registry.
+     *
+     * @param string $identifier
+     * @param null $default
+     *
+     * @return mixed
+     */
+    public function getContentById($identifier, $default = null)
+    {
+        $this->assertion->keyExists(
+            $this->registry[$this->section],
+            $identifier,
+            'Requested item ('. $identifier .') is not registered in the current registry.'
+        );
+
+        return $this->registry[$this->section][$identifier];
+    }
+
+    /**
+     * Determines if the given identifier refers to a world.
+     *
+     * @param string $identifier
+     * @return bool
+     *
+     * @throws \Assert\InvalidArgumentException in case the $identifier is not a string.
+     */
+    public function isRegistered($identifier)
+    {
+        $this->verifySectionName($identifier);
+
+        return array_key_exists($identifier, $this->registry[$this->section]);
+    }
+
+    /**
+     * Adds an item to the register.
+     *
+     * @param string $identifier
+     * @param mixed $value
+     * @throws RegistryException
+     * @return void
+     */
+    public function register($identifier, $value)
+    {
+        if ($this->isRegistered($identifier)) {
+            throw new RegistryException(
+                $this->drupalCommonConnector->t(RegistryException::DUPLICATE_REGISTRATION_ATTEMPT_TEXT),
+                RegistryException::DUPLICATE_REGISTRATION_ATTEMPT_CODE
+            );
+        }
+
+        $this->registry[$this->section][$identifier] = $value;
+    }
+
+    /**
+     * Replaces the content of the item identified by it's registration key by the new value.
+     *
+     * @param string $identifier
+     * @param mixed $value
+     * @throws \Liip\Drupal\Modules\Registry\RegistryException
+     */
+    public function replace($identifier, $value)
+    {
+        if (!$this->isRegistered($identifier)) {
+            throw new RegistryException(
+                $this->drupalCommonConnector->t(RegistryException::MODIFICATION_ATTEMPT_FAILED_TEXT),
+                RegistryException::MODIFICATION_ATTEMPT_FAILED_CODE
+            );
+        }
+
+        $this->registry[$this->section][$identifier] = $value;
+    }
+
+    /**
+     * Removes an item off the register.
+     *
+     * @param string $identifier
+     */
+    public function unregister($identifier)
+    {
+        if (!$this->isRegistered($identifier)) {
+            throw new RegistryException(
+                $this->drupalCommonConnector->t(RegistryException::UNKNOWN_IDENTIFIER_TEXT),
+                RegistryException::UNKNOWN_IDENTIFIER_CODE
+            );
+        }
+        unset($this->registry[$this->section][$identifier]);
     }
 }
