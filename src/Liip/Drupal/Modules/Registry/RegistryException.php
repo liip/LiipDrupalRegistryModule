009a61d646f791fbfe3623f292bd05a2a9dac408
Bastian Feder <github@bastian-feder.de>
2013-03-16 11:23:07 +0100

    Added missing constants

--- src/Liip/Drupal/Modules/Registry/RegistryException.php
+++ src/Liip/Drupal/Modules/Registry/RegistryException.php
@@ -6,9 +6,12 @@ class RegistryException extends \Exception
     const DUPLICATE_REGISTRATION_ATTEMPT_CODE = 1;
     const DUPLICATE_REGISTRATION_ATTEMPT_TEXT = "Given identifier already applies to an item of the registry. Please choose a different one.";
 
-    const MODIFICATION_ATTEMPT_FAILED_CODE = 1;
+    const MODIFICATION_ATTEMPT_FAILED_CODE = 2;
     const MODIFICATION_ATTEMPT_FAILED_TEXT = "Given identifier does not applies to an item of the registry.";
 
-    const UNKNOWN_IDENTIFIER_CODE = 2;
+    const UNKNOWN_IDENTIFIER_CODE = 3;
     const UNKNOWN_IDENTIFIER_TEXT = "Given identifier does not match a registered one.";
+
+    const DUPLICATE_INITIATION_ATTEMPT_CODE = 4;
+    const DUPLICATE_INITIATION_ATTEMPT_TEXT = "Registry does already exist!";
 }
