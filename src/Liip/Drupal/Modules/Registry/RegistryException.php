e103ac9de759a7781da7c92672144adf63aa7993
Bastian Feder <github@bastian-feder.de>
2013-09-12 10:25:28 +0200

    Corrected error in error text.

--- src/Liip/Drupal/Modules/Registry/RegistryException.php
+++ src/Liip/Drupal/Modules/Registry/RegistryException.php
@@ -7,7 +7,7 @@ class RegistryException extends \Exception
     const DUPLICATE_REGISTRATION_ATTEMPT_TEXT = "Given identifier already applies to an item of the registry. Please choose a different one.";
 
     const MODIFICATION_ATTEMPT_FAILED_CODE = 2;
-    const MODIFICATION_ATTEMPT_FAILED_TEXT = "Given identifier does not applies to an item of the registry.";
+    const MODIFICATION_ATTEMPT_FAILED_TEXT = "Given identifier does not apply to an item of the registry.";
 
     const UNKNOWN_IDENTIFIER_CODE = 3;
     const UNKNOWN_IDENTIFIER_TEXT = "Given identifier does not match a registered one.";
