ff6fc5ebb871b070f957d0b8c90e1425f3eb5aef
Bastian Feder <github@bastian-feder.de>
2013-09-22 19:14:10 +0200

    Tested and Refactored Multiply registry

--- Tests/Liip/Drupal/Modules/Registry/MultiplyTest.php
+++ Tests/Liip/Drupal/Modules/Registry/MultiplyTest.php
@@ -17,18 +17,14 @@ class MultiplyTest extends RegistryTestCase
     /**
      * Provides an instance of the Multiply registry.
      *
-     * @param $section
+     * @param string $section
+     * @param array $registries
      *
      * @return Multiply
      */
-    protected function getMultiplyObject($section = 'testSection')
+    protected function getMultiplyObject($section = 'testSection', $registries = array('D7Config','Popo'))
     {
         $assertion = $this->getAssertionObjectMock(array('notEmpty'));
-
-        $registries = array(
-            'D7Config',
-            'Popo',
-        );
 
         return new Multiply($section, $assertion, $registries);
     }
@@ -75,7 +71,7 @@ class MultiplyTest extends RegistryTestCase
         $factory = $this->getMockBuilder('\Liip\Drupal\Modules\Registry\Factory')
             ->disableOriginalConstructor()
             ->setMethods($methods)
-            ->getMockForAbstractClass();
+            ->getMock();
 
         return $factory;
     }
@@ -150,6 +146,178 @@ class MultiplyTest extends RegistryTestCase
     }
 
     /**
+     * @covers \Liip\Drupal\Modules\Registry\Multiply::register
+     */
+    public function testRegister()
+    {
+        $dispatcher = $this->getMockBuilder('\Liip\Drupal\Modules\Registry\Dispatcher')
+            ->setMethods(array('dispatch', 'hasError'))
+            ->getMock();
+        $dispatcher
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with(
+                'register',
+                $this->isType('string'),
+                $this->isType('array')
+            );
+        $dispatcher
+            ->expects($this->once())
+            ->method('hasError')
+            ->will($this->returnValue(false));
+
+        $multiply = $this->getMultiplyObject();
+        $multiply->setDispatcher($dispatcher);
+
+        $multiply->register('Tux', array('id' => 'tux'));
+
+        $this->assertEquals(array('id' => 'tux'), $multiply->getContentById('Tux'));
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Multiply::register
+     */
+    public function testRegisterExpectingException()
+    {
+        $dispatcher = $this->getMockBuilder('\Liip\Drupal\Modules\Registry\Dispatcher')
+            ->setMethods(array('dispatch', 'hasError'))
+            ->getMock();
+        $dispatcher
+            ->expects($this->exactly(2))
+            ->method('hasError')
+            ->will($this->returnValue(true));
+
+        $multiply = $this->getMultiplyObject();
+        $multiply->setDispatcher($dispatcher);
+
+        $this->setExpectedException('\Liip\Drupal\Modules\Registry\RegistryException');
+
+        $multiply->register('Tux', array('id' => 'tux'));
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Multiply::replace
+     */
+    public function testReplace()
+    {
+        $dispatcher = $this->getMockBuilder('\Liip\Drupal\Modules\Registry\Dispatcher')
+            ->setMethods(array('dispatch', 'hasError'))
+            ->getMock();
+        $dispatcher
+            ->expects($this->exactly(2))
+            ->method('hasError')
+            ->will($this->returnValue(false));
+
+        $multiply = $this->getMultiplyObject('testSection', array('Popo'));
+        $multiply->setDispatcher($dispatcher);
+
+        $multiply->register('Tux', array('id' => 'tux'));
+        $multiply->replace('Tux', array('id' => 'tux', 'data' => array()));
+
+        $this->assertEquals(array('id' => 'tux', 'data' => array()), $multiply->getContentById('Tux'));
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Multiply::replace
+     */
+    public function testReplaceExpectingException()
+    {
+        $dispatcher = $this->getMockBuilder('\Liip\Drupal\Modules\Registry\Dispatcher')
+            ->setMethods(array('dispatch', 'hasError'))
+            ->getMock();
+        $dispatcher
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with(
+                $this->isType('string'),
+                $this->isType('string'),
+                $this->isType('array')
+            );
+        $dispatcher
+            ->expects($this->exactly(2))
+            ->method('hasError')
+            ->will($this->returnValue(true));
+
+        $multiply = $this->getMultiplyObject('testSection', array('Popo'));
+        $multiply->setDispatcher($dispatcher);
+
+        $this->setExpectedException('\Liip\Drupal\Modules\Registry\RegistryException');
+
+        $multiply->replace('Tux', array('id' => 'tux', 'data' => array()));
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Multiply::unregister
+     */
+    public function testUnregister()
+    {
+        $dispatcher = $this->getMockBuilder('\Liip\Drupal\Modules\Registry\Dispatcher')
+            ->setMethods(array('dispatch', 'hasError'))
+            ->getMock();
+
+        $dispatcher
+            ->expects($this->exactly(2))
+            ->method('hasError')
+            ->will($this->returnValue(false));
+
+        $multiply = $this->getMultiplyObject('testSection', array('Popo'));
+        $multiply->setDispatcher($dispatcher);
+
+        $multiply->register('Tux', array('id' => 'tux'));
+        $multiply->unregister('Tux');
+
+        $this->assertFalse($multiply->isRegistered('Tux'));
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Multiply::unregister
+     */
+    public function testUnregisterExpectingException()
+    {
+        $dispatcher = $this->getMockBuilder('\Liip\Drupal\Modules\Registry\Dispatcher')
+            ->setMethods(array('dispatch', 'hasError'))
+            ->getMock();
+
+        $dispatcher
+            ->expects($this->exactly(2))
+            ->method('hasError')
+            ->will($this->returnValue(true));
+
+        $multiply = $this->getMultiplyObject('testSection', array('Popo'));
+        $multiply->setDispatcher($dispatcher);
+
+        $this->setExpectedException('\Liip\Drupal\Modules\Registry\RegistryException');
+
+        $multiply->unregister('Tux');
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Multiply::isRegistered
+     */
+    public function testIsRegistered()
+    {
+        $multiple = $this->getMultiplyObject();
+        $multiple->setFactory($this->getFactoryReturningNoContent());
+
+        $this->assertFalse($multiple->isRegistered('tux'));
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Multiply::isRegistered
+     */
+    public function testIsRegisteredFromCache()
+    {
+        $multiple = $this->getProxyBuilder('\Liip\Drupal\Modules\Registry\Multiply')
+            ->setConstructorArgs(array('testSection', $this->getAssertionObjectMock(), array('Popo')))
+            ->setProperties(array('registry'))
+            ->getProxy();
+        $multiple->registry = array('testSection' => array('tux' => array()));
+        $multiple->setFactory($this->getFactoryReturningNoContent());
+
+        $this->assertTrue($multiple->isRegistered('tux'));
+    }
+
+    /**
      * @covers \Liip\Drupal\Modules\Registry\Multiply::getLogger
      * @covers \Liip\Drupal\Modules\Registry\Multiply::setLogger
      */
@@ -181,5 +349,27 @@ class MultiplyTest extends RegistryTestCase
 
         $multiple->setFactory($factory);
         $this->assertSame($factory, $multiple->getFactory());
+    }
+
+    /**
+     * @covers \Liip\Drupal\Modules\Registry\Multiply::getDispatcher
+     * @covers \Liip\Drupal\Modules\Registry\Multiply::setDispatcher
+     */
+    public function testDispatcher()
+    {
+        $dispatcher = $this->getMock('\Liip\Drupal\Modules\Registry\Dispatcher');
+        $factory = $this->getFactoryStub(array('getRegistry'));
+        $factory
+            ->expects($this->once())
+            ->method('getRegistry')
+            ->will($this->returnValue($this->getRegistryStub('\Liip\Drupal\Modules\Registry\Memory\Popo')));
+
+        $multiple = $this->getMultiplyObject('testSection', array('Popo'));
+        $multiple->setFactory($factory);
+
+        $this->assertInstanceOf('\Liip\Drupal\Modules\Registry\Dispatcher', $multiple->getDispatcher());
+
+        $multiple->setDispatcher($dispatcher);
+        $this->assertSame($dispatcher, $multiple->getDispatcher());
     }
 }
