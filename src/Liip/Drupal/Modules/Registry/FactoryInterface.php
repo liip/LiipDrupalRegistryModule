5d27ab5002d984019520195089be22e852f60813
Bastian Feder <github@bastian-feder.de>
2013-09-23 14:44:09 +0200

    Documented: added missing Docblock

--- src/Liip/Drupal/Modules/Registry/FactoryInterface.php
+++ src/Liip/Drupal/Modules/Registry/FactoryInterface.php
@@ -7,7 +7,15 @@ use Assert\Assertion;
 
 interface FactoryInterface
 {
-
+    /**
+     * Provides an object of the requested implementation of the RegistryInterface.
+     *
+     * @param string $name
+     * @param string $section
+     * @param Assertion $assertion
+     *
+     * @return RegistryInterface
+     */
     public function getRegistry($name, $section, Assertion $assertion);
 
 }
